5a1406b5b1b74058eacf2a0feb7decf76dd9e98b
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-07-15 15:44:55 +0200

    [ADD] styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `myBooking`,
+    description: `come where you want in few clicks!`,
+    author: `Alexandre Ohayon`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
