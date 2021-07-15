83bb4f375902f4f8d51c6acea4767952f057f746
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-07-15 16:28:31 +0200

    [ADD] global style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import styled from 'styled-components'
-
+import {FaBars} from 'react-icons/fa'
 const Header = () => {
   return (
     <Nav>
       <NavLink to="/">myBooking</NavLink>
+      <Bars />
     </Nav>
   )
 }
@@ -13,10 +14,36 @@ const Header = () => {
 export default Header
 
 const Nav = styled.nav`
-  background: transparent;
+  background: red;
   height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1300px) / 2);
+  z-index: 100;
+  position: relative;
 `
 
 const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+`
+
+const Bars = styled(FaBars)`
+  color: white;
+  display: none;
   
+  @media screen and (max-width: 768px) {
+      display: block;
+      position: absolute;
+      top: 0;
+      right: 0;
+      transform: translate(-100%, 75%);
+      font-size: 1.8rem;
+      cursor: pointer;
+  }
 `
